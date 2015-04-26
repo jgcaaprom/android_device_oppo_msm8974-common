ff513e1d5bc153fdd4083e7fae8ef2bb9e5f44e2
jgcaaprom <jgcaap@live.com>
2015-04-26 15:00:49 +0100

    BoardConfigCommon: Proved not important.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -175,9 +175,7 @@ BOARD_SEPOLICY_DIRS += \
 -include vendor/oppo/msm8974-common/BoardConfigVendor.mk
 
 #Rom Optimizations
-ENABLE_GCCONLY := true
 USE_HOST_4_8 := true
 USE_O3_OPTIMIZATIONS := true
 KRAIT_TUNINGS := true
-TARGET_USE_PIPE := true
 STRICT_ALIASING := true
